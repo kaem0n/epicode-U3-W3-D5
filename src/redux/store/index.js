fae077af1c812f42dada1311520b07703e181685
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-02-16 12:50:38 +0100

    finished main task

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,5 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import reducer from '../reducers'
-
-// const globalReducer = combineReducers({
-//   something: () => {
-//     console.log('yeah!')
-//   },
-//   moreOfIt: () => {
-//     console.log('yeah!')
-//   },
-// })
 
 const store = configureStore({
   reducer: reducer,
